33af5b062a47300bf06060c78f8709d7736dd814
ScriptAndSyntax <148912837+PHARMADICT@users.noreply.github.com>
2026-07-05 20:36:01 +0400

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // This loads the HTML file you created
+  mainWindow.loadFile('index.html');
+  
+  // This opens the window in full screen
+  mainWindow.maximize();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
